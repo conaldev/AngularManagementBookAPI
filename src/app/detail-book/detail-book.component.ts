c28a39e45a3d6ec9f828f6944db1a3fdd0a5367d
conaldev <talaaidoaibiet@gmail.com>
2020-10-30 15:57:57 +0700

    v1

--- src/app/detail-book/detail-book.component.ts
+++ src/app/detail-book/detail-book.component.ts
@@ -1,4 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Book} from '../book';
+import {BookService} from '../service/book.service';
+import {ActivatedRoute} from '@angular/router';
+import {throwError} from 'rxjs';
 
 @Component({
   selector: 'app-detail-book',
@@ -7,9 +11,20 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class DetailBookComponent implements OnInit {
 
-  constructor() { }
+  book: Book;
 
-  ngOnInit(): void {
+  constructor(private bookService: BookService,
+              private route: ActivatedRoute) {
   }
 
+  ngOnInit(): void {
+    // @ts-ignore
+    // tslint:disable-next-line:radix
+    const id = Number.parseInt(this.route.snapshot.paramMap.get('id'));
+    this.bookService.getBookById(id).subscribe(data => {
+      this.book = data;
+    });
+  }
+
+  // tslint:disable-next-line:typedef
 }
